0b09da48a888ab13bd7007ce402c6420ce814064
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-01-21 22:55:46 +0530

    Solve exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    result = (5 / 9) * (temp - 32);
+    return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    result = (9 * temp / 5) + 32;
+    return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
