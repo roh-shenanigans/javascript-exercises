0f8fffacf938d1e488c93032e9041235ab53bcc4
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-01-21 21:27:17 +0530

    Solve exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (...args) {
+    let arr = args[0];
+    let items = args.slice(0, args.length)
 
+    let isKept = function (item) {
+        return !items.includes(item);
+    }
+
+    return arr.filter(isKept);
+}
+
+// console.log(removeFromArray([1, 2, 3, 4], 3))
+
+// Solved
 // Do not edit below this line
 module.exports = removeFromArray;
