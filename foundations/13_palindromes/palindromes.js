9551e3dddf0be18c40f772ec78aac94b2d26de01
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-02-09 15:07:04 +0530

    Solve exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let word = string.toLowerCase().split("").filter(isAlphaNumeric).join("");
+    let reversed = word.split("").reverse().join("");
+    // console.log(word);
+    // console.log(reversed);
+    return (word === reversed);
 };
+
+function isAlphaNumeric(str) {
+    const regex = /^[a-z0-9]+$/i;
+    return regex.test(str);
+}
+
+console.log(palindromes('A car, a man, a maraca.'));
+// palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
