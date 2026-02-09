5cc027dc0df77a9bd5fab01c439461f39a385087
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-02-09 13:50:02 +0530

    Solve exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numList) {
+    return numList.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+    return numList.reduce((total, currentItem) => {
+        return total * currentItem;
+    }, 1)
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+    let result = 1;
+
+    for (let i = 0; i < exp; i++) {
+        result *= base;
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let result = 1;
+
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
