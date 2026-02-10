9d8bb4c7ff5c696886f61286194b0e1ace7b002a
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-02-10 12:53:31 +0530

    Solve exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(personList) {
+    let oldestPerson;
+    let oldestAge = 0;
+    const today = new Date();
+    const currentYear = today.getFullYear();
 
+    for (const person of personList) {
+        let age = 0;
+
+        if (person.hasOwnProperty("yearOfDeath")) {
+            age = person.yearOfDeath - person.yearOfBirth;
+
+        } else {
+            age = currentYear - person.yearOfBirth;
+        }
+
+        if (oldestPerson === undefined) {
+            oldestPerson = person;
+        }
+
+        if (age > oldestAge) {
+            oldestPerson = person;
+            oldestAge = age;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
