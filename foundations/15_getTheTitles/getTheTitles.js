ef8b17ab6608b68dc91159961755ce18adf62353
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-02-10 12:36:11 +0530

    Solve exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const getTheTitles = function (bookList) {
+    let titles = [];
 
+    for (const book of bookList) {
+        if (book.hasOwnProperty("title")) {
+            console.log(book.title)
+            titles.push(book.title);
+    	}
+    }
+
+    return titles;
 };
 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+// console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
